bce84b010b3b7271af3e7468c0face470c0460ce
ArthurRep <a.rep@chello.nl>
2025-01-30 19:52:53 +0100

    first commit

---
+++ pwm_plotter.ino
@@ -0,0 +1,21 @@
+/* pwm_plotter
+   Laat pwm-signaal zien op plotter.
+   Verbind pin 3 met A3.
+   Voor RC 4,7k en 22 uF naar A7
+   Gebruik Arduino 1.8 IDE
+*/
+
+void setup() {
+  Serial.begin(230400);
+  pinMode(3, OUTPUT);
+  analogWrite(3, 128);
+}
+
+void loop() {
+  unsigned long tijd = millis();
+  do {
+    //Serial.println(analogRead(A3));
+    Serial.println(analogRead(A7)); // met RC-netwerkje
+  } while (millis() <= tijd + 10);
+  while (1);
+}
